ce524c18ea969f0619cee7bb30def4599304b6d9
unartig <jurinho@hotmail.de>
2024-11-29 18:14:47 +0100

    WIP shared kernel

--- gpu_nbody.cu
+++ gpu_nbody.cu
@@ -15,7 +15,9 @@
 #include <random>
 #include <thread>
 
-// #define G 6.67408e-11
+#define G 6.67408e-11
+#define BLOCK_SIZE 128
+#define N 2e3
 
 int width = 1280;
 int height = 720;
@@ -34,71 +36,37 @@ int height = 720;
 // Kernel for updating positions and velocities
 __global__ void step(float *positions, float *velocities, float *masses,
                      int pointCount, int totalPairs) {
-  __shared__ int pairs;
-  pairs = 0;
-  __syncthreads();
-  int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  float G = 6.67408e-11 * 1000;
-  if (idx < totalPairs) {
-    int i = 0;
-    int sum = 0;
-    while (sum + (pointCount - 1 - i) <= idx) {
-        sum += (pointCount - 1 - i);
-        i++;
-    }
-
-    // Step 2: Find j
-    int j = idx - sum + i + 1;
-
-    if (i!=j){
-      pairs++;
-      // Calcualate Interaction
+  int pi = blockIdx.x * blockDim.x + threadIdx.x;
+  __shared__ float pos_s[BLOCK_SIZE];  
+  for (int i = 0; i < N; i += blockDim.x) {
+    pos_s[threadIdx.x] = positions[(int)(i/N) * blockDim.x + threadIdx.x];
+    __syncthreads();
+    for (int j = 0; j < BLOCK_SIZE;j++){
       float dx = positions[j * 3] - positions[i * 3];
       float dy = positions[j * 3 + 1] - positions[i * 3 + 1];
       float dz = positions[j * 3 + 2] - positions[i * 3 + 2];
-
       float distSq = dx * dx + dy * dy + dz * dz;
       if (distSq < 1e-4f)
         distSq = 1e-4f;
 
       float invDist = 1.0f / sqrtf(distSq);
       float invDist3 = invDist * invDist * invDist;
-
       float force = G * masses[i] * masses[j] * invDist3;
-      // if(i==0 && j == 1){
-      //   printf("P1%.25f %.25f %.25f\n", positions[0], positions[1],positions[2]);
-      //   printf("P2%.25f %.25f %.25f\n", positions[3], positions[4],positions[5]);
-      //   printf("D%.25f\n", invDist3);
-      //   printf("G%.25f\n", G);
-      //   printf("F%.25f\n", force);
-      // }
       velocities[i * 3] += dx * force / masses[i];
       velocities[i * 3 + 1] += dy * force / masses[i];
       velocities[i * 3 + 2] += dz * force / masses[i];
-
-      velocities[j * 3] -= dx * force / masses[j];
-      velocities[j * 3 + 1] -= dy * force / masses[j];
-      velocities[j * 3 + 2] -= dz * force / masses[j];
     }
   }
-
-  // printf("%d = %d\n", pairs, pointCount*pointCount);
-  // __syncthreads();
-  // if (idx < pointCount) {
-  //   positions[idx * 3] += velocities[idx * 3];
-  //   positions[idx * 3 + 1] += velocities[idx * 3 + 1];
-  //   positions[idx * 3 + 2] += velocities[idx * 3 + 2];
-  // }
+  __syncthreads();
 }
 __global__ void update(float *positions, float *velocities, int pointCount) {
   int i = blockIdx.x * blockDim.x + threadIdx.x;
   if (i < pointCount) {
-      positions[i * 3] += velocities[i * 3];
-      positions[i * 3 + 1] += velocities[i * 3 + 1];
-      positions[i * 3 + 2] += velocities[i * 3 + 2];
+    positions[i * 3] += velocities[i * 3];
+    positions[i * 3 + 1] += velocities[i * 3 + 1];
+    positions[i * 3 + 2] += velocities[i * 3 + 2];
   }
 }
-
 
 // Function to compile shader
 GLuint compileShader(GLenum type, const char *source) {
@@ -214,7 +182,6 @@ int main() {
   ImGuiIO &io = ImGui::GetIO();
   io.Fonts->AddFontDefault(&fontConfig);
 
-  const int N = 2e3;
   float *positions, *velocities, *masses;
   cudaMallocHost(&positions, N * 3 * sizeof(float));
   cudaMallocHost(&velocities, N * 3 * sizeof(float));
@@ -254,11 +221,14 @@ int main() {
   cudaMalloc(&d_positions, N * 3 * sizeof(float));
   cudaMalloc(&d_velocities, N * 3 * sizeof(float));
   cudaMalloc(&d_masses, N * sizeof(float));
-  cudaMemcpy(d_positions, positions, N * 3 * sizeof(float), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_velocities, velocities, N * 3 * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_positions, positions, N * 3 * sizeof(float),
+             cudaMemcpyHostToDevice);
+  cudaMemcpy(d_velocities, velocities, N * 3 * sizeof(float),
+             cudaMemcpyHostToDevice);
   cudaMemcpy(d_masses, masses, N * sizeof(float), cudaMemcpyHostToDevice);
 
-  GLuint shaderProgram = createShaderProgram(vertexShaderSource, fragmentShaderSource);
+  GLuint shaderProgram =
+      createShaderProgram(vertexShaderSource, fragmentShaderSource);
   if (shaderProgram == 0) {
     fprintf(stderr, "Failed to create shader program\n");
     glfwDestroyWindow(window);
@@ -271,7 +241,8 @@ int main() {
 
   glBindVertexArray(vao);
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, N * 3 * sizeof(float), positions, GL_DYNAMIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, N * 3 * sizeof(float), positions,
+               GL_DYNAMIC_DRAW);
 
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
@@ -280,12 +251,13 @@ int main() {
   glBindVertexArray(0);
 
   float aspectRatio = static_cast<float>(width) / static_cast<float>(height);
-  
+
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
-    
+
     double currentTime = glfwGetTime();
-    cudaMemcpy(positions, d_positions, N * 3 * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(positions, d_positions, N * 3 * sizeof(float),
+               cudaMemcpyDeviceToHost);
 
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
@@ -295,14 +267,14 @@ int main() {
     glBindVertexArray(vao);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(positions), positions);
-    glBufferData(GL_ARRAY_BUFFER, N * 3 * sizeof(float), positions, GL_DYNAMIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, N * 3 * sizeof(float), positions,
+                 GL_DYNAMIC_DRAW);
     glEnable(GL_BLEND);
     glEnable(GL_VERTEX_PROGRAM_POINT_SIZE);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE);
     glDrawArrays(GL_POINTS, 0, N);
     glLoadIdentity();
     glBindVertexArray(0);
-
 
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
@@ -318,30 +290,28 @@ int main() {
             ImGuiWindowFlags_NoSavedSettings |
             ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav);
 
-
     // glfwSwapBuffers(window);
 
-    int blockSize = 128;
-    int numBlocks = (N*N + blockSize - 1) / blockSize;
+    int numBlocks = (N * N + BLOCK_SIZE - 1) / BLOCK_SIZE;
     // printf("NBlocks %d\n", numBlocks);
     int totalPairs = (N * (N - 1)) / 2;
-    int numPairBlocks = (totalPairs + blockSize - 1) / blockSize;
+    int numPairBlocks = (totalPairs + BLOCK_SIZE - 1) / BLOCK_SIZE;
 
-    step<<<numPairBlocks, blockSize>>>(d_positions, d_velocities, d_masses, N, totalPairs);
+    step<<<numPairBlocks, BLOCK_SIZE>>>(d_positions, d_velocities, d_masses, N,
+                                        totalPairs);
     cudaDeviceSynchronize();
     cudaCheckErrors("STEP Kernel execution failed");
-    update<<<numBlocks, blockSize>>>(d_positions, d_velocities, N);
+    update<<<numBlocks, BLOCK_SIZE>>>(d_positions, d_velocities, N);
     cudaDeviceSynchronize();
     cudaCheckErrors("UPDATE Kernel execution failed");
     ImGui::End();
 
-    
     double frameTime = glfwGetTime() - currentTime;
     ImGui::SetNextWindowSize(ImVec2(200, 70));
     ImGui::SetNextWindowPos(ImVec2(10, 10));
     ImGui::Begin("Performance");
     ImGui::Text("Frame time: %.3f ms", frameTime * 1000);
-    ImGui::Text("FPS: %.3f", 1000/(frameTime * 1000));
+    ImGui::Text("FPS: %.3f", 1000 / (frameTime * 1000));
     ImGui::End();
     ImGui::Render();
 
